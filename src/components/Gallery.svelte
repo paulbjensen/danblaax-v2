d9e7308b1c94a6b091a4e9c8857b31e4758ff560
Paul Jensen <paulbjensen@gmail.com>
2025-12-16 15:02:41 +0000

    Added a slider carousel for the photos

---
+++ src/components/Gallery.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { Slidy, type Slide } from "@slidy/svelte";
+  import "@slidy/svelte/dist/slidy.css";
+
+  export let slides: Slide[] = [];
+  export let options = {
+    arrows: true,
+    navigation: false,
+    progress: false,
+    thumbnail: true,
+    counter: false,
+  };
+</script>
+
+<Slidy {slides} {...options} />
